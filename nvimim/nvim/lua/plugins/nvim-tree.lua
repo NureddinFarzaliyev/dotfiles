f67590dabb4c13368a0eee52e8dd672437476a60
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-05-24 16:05:10 +0400

    Nvim Tree keymaps

--- nvimim/nvim/lua/plugins/nvim-tree.lua
+++ nvimim/nvim/lua/plugins/nvim-tree.lua
@@ -1,10 +1,11 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons", -- optional, for file icons
-  },
-  config = function()
-    require("nvim-tree").setup()
-  end,
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons", -- optional, for file icons
+	},
+	config = function()
+		require("nvim-tree").setup()
+		require("keymaps.nvim-tree")
+	end,
 }
