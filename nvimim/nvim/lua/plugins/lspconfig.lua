b645f6d529e5d8b92ec244ffe7d235dff81c2c71
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-05-24 13:49:49 +0400

    Configuring LSPs

---
+++ nvimim/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,11 @@
+return {
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+
+		lspconfig.ts_ls.setup({})
+	end,
+}
---
+++ nvimim/nvim/lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,22 @@
+return {
+  "WhoIsSethDaniel/mason-tool-installer.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+  },
+  config = function()
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        -- LSP servers
+        "typescript-language-server",
+
+        -- Formatters
+        -- "prettierd",
+
+        -- Linters
+        -- "eslint_d",
+      },
+      auto_update = false,
+      run_on_start = true,
+    })
+  end,
+}
