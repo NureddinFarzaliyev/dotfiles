66358a60d0849e005dc4ebebd0c95d7356f0141e
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-05-25 12:22:56 +0400

    Clipboard & inline errors

--- nvimim/nvim/init.lua
+++ nvimim/nvim/init.lua
@@ -11,6 +11,16 @@ vim.opt.expandtab = true
 vim.opt.number = true
 vim.opt.fillchars:append({ eob = " " }) -- Remove end of buffer message (~)
 
+vim.opt.clipboard = "unnamedplus"
+
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+})
+
 require("config.lazy")
 require("config.autocmds")
 require("keymaps.keymaps")
