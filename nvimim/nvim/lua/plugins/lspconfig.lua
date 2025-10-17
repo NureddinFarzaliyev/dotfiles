5cec64c00328a78d5ba0171bf4894c6091d09f0d
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-10-17 11:19:28 +0400

    add vue lsp to nvim

--- nvimim/nvim/lua/plugins/lspconfig.lua
+++ nvimim/nvim/lua/plugins/lspconfig.lua
@@ -7,14 +7,24 @@ return {
 		local lspconfig = require("lspconfig")
 		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+    local vue_ls_path = vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server/node_modules")
+
 		lspconfig.ts_ls.setup({
 			capabilities = capabilities,
 			init_options = {
+        plugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vue_ls_path,
+            languages = { "vue" },
+          }
+        },
 				preferences = {
 					importModuleSpecifierPreference = "non-relative",
 					importModuleSpecifierEnding = "auto",
 				},
 			},
+      filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact", "vue" },
 		})
 		lspconfig.cssls.setup({
 			capabilities = capabilities,
