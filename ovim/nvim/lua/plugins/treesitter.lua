d74d328b9a0c9183b8d95e3f51be9e90cbbf081f
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-12-06 09:52:56 +0400

    added go treesitter to ovim

--- ovim/nvim/lua/plugins/treesitter.lua
+++ ovim/nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "javascript", "lua", "python", "vue", "html", "css", "bash" },
+      ensure_installed = { "javascript", "lua", "python", "vue", "html", "css", "bash", "go" },
         highlight = {
           enable = true,
         },
